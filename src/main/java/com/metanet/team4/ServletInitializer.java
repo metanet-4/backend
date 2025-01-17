89bbd056a913ebcf9255c804900b9200f83e5e9e
kokomong2 <ko9603@naver.com>
2025-01-17 10:20:07 +0900

    chore: 프로젝트 뼈대 생성

---
+++ src/main/java/com/metanet/team4/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.metanet.team4;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Team4Application.class);
+	}
+
+}
