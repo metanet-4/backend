2c8a065316114d81ed8aba137f09a93c7c94ea47
SeungJun Go <105098279+kokomong2@users.noreply.github.com>
2025-02-02 17:44:30 +0900

    [feat] pr 시 테스트 돌아가는 기능 추가 (#24)
    * refactor: jacoco 버전 수정
    
    * feat: pr, push 시 테스트 코드 작동 기능 추가
    
    * feat: db 연결 체크 하는 health api 추가
    
    * refactor: health/db도 권한 없이 사용 가능하게 수정
    
    * feat: test시 metanet4 폴더에서 돌리도록 수정
    
    * feat: pr과 push 시에 application.properties 생성 코드 추가
    
    * feat: push 시에 application.properties 생성하는 코드 추가

--- metanet4/src/main/java/com/metanet/team4/health/HealthCheckController.java
+++ metanet4/src/main/java/com/metanet/team4/health/HealthCheckController.java
@@ -11,12 +11,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/health")
 public class HealthCheckController {
+	
+    private final HealthCheckMapper healthCheckMapper;
+
+    public HealthCheckController(HealthCheckMapper healthCheckMapper) {
+        this.healthCheckMapper = healthCheckMapper;
+    }
 
     @GetMapping
     public ResponseEntity<Map<String, String>> healthCheck() {
         Map<String, String> healthStatus = new HashMap<>();
         healthStatus.put("status", "healthy");
         healthStatus.put("database", "connected"); // 데이터베이스 상태 추가
+        return ResponseEntity.ok(healthStatus);
+    }
+    
+    @GetMapping("/db")
+    public ResponseEntity<Map<String, String>> healthDBCheck() {
+        Map<String, String> healthStatus = new HashMap<>();
+        healthStatus.put("status", "healthy");
+
+        try {
+            int employeeCount = healthCheckMapper.getEmployeeCount();
+            healthStatus.put("database", "connected");
+            healthStatus.put("employee_count", String.valueOf(employeeCount));
+        } catch (Exception e) {
+            healthStatus.put("database", "error");
+        }
+
         return ResponseEntity.ok(healthStatus);
     }
 }
