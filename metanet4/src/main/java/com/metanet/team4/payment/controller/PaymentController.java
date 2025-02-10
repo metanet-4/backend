4f4c10833334727936aaf04ac77b3347579e7127
seung-hun2 <86557591+seung-hun2@users.noreply.github.com>
2025-02-10 12:33:41 +0900

    feat: 결제완료 시 알림 전송 (#72)

--- metanet4/src/main/java/com/metanet/team4/payment/controller/PaymentController.java
+++ metanet4/src/main/java/com/metanet/team4/payment/controller/PaymentController.java
@@ -1,9 +1,12 @@
 package com.metanet.team4.payment.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.metanet.team4.common.Login;
@@ -11,6 +14,7 @@ import com.metanet.team4.member.model.Member;
 import com.metanet.team4.payment.model.PaymentRequestDto;
 import com.metanet.team4.payment.model.PaymentResponseDto;
 import com.metanet.team4.payment.service.PaymentService;
+import com.metanet.team4.websocket.WebSocketService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -24,13 +28,20 @@ import lombok.RequiredArgsConstructor;
 public class PaymentController {
 
     private final PaymentService paymentService;
+    private final WebSocketService webSocketService;
 
     @Operation(summary = "결제 요청", description = "사용자가 결제를 요청하면 승인 결과를 반환합니다.")
     @PostMapping
     public ResponseEntity<PaymentResponseDto> requestPayment(
             @Parameter(description = "결제 요청 정보", required = true) @RequestBody PaymentRequestDto request,
             @Parameter(hidden = true) @Login Member member) {
-        return ResponseEntity.ok(paymentService.processPayment(request, member));
+    	PaymentResponseDto payment = paymentService.processPayment(request, member); 
+    	if(payment != null) {
+    		String message = member.getUserId() + "님 예매가 완료되었습니다.";
+     		webSocketService.sendNotificationToUser(member.getUserId(), message);
+    	}
+    	
+        return ResponseEntity.ok(payment);
     }
 }
 
