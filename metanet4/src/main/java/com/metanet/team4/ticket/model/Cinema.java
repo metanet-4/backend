f4c74b5517789cb87fe3c16971f5aafb24b192b6
seung-hun2 <jungsh2328@naver.com>
2025-02-10 11:44:13 +0900

    feat: 영화관, 상영관, 좌석 선택 model

---
+++ metanet4/src/main/java/com/metanet/team4/ticket/model/Cinema.java
@@ -0,0 +1,9 @@
+package com.metanet.team4.ticket.model;
+
+import lombok.Getter;
+
+@Getter
+public class Cinema {
+	private String name;
+	private String location;
+}
---
+++ metanet4/src/main/java/com/metanet/team4/ticket/model/Seat.java
@@ -0,0 +1,10 @@
+package com.metanet.team4.ticket.model;
+
+import lombok.Getter;
+
+@Getter
+public class Seat {
+	private Long id;
+	private String name;
+	private Long screenId;
+}
