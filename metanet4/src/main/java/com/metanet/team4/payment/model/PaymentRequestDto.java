68e023c47d4baac696bf9c3cbb11f5c48273b25c
kokomong2 <ko9603@naver.com>
2025-02-11 10:51:19 +0900

    feat: requestDto null 허용 안하게 하고 valid 추가

--- metanet4/src/main/java/com/metanet/team4/payment/model/PaymentRequestDto.java
+++ metanet4/src/main/java/com/metanet/team4/payment/model/PaymentRequestDto.java
@@ -1,5 +1,7 @@
 package com.metanet.team4.payment.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,11 +10,19 @@ import lombok.ToString;
 @Setter
 @ToString
 public class PaymentRequestDto {
-	
-	private Long playingId;
-	private String receiptId;
-	
-	private Integer paymentAmount;
-    private String ticketType; // 티켓 유형 (일반, 청소년 등)
-    private String seatNames; // 좌석 정보(E3, E4)
+
+    @NotNull(message = "상영 ID는 필수 값입니다.")
+    private Long playingId;
+
+    @NotBlank(message = "영수증 ID는 필수 값입니다.")
+    private String receiptId;
+
+    @NotNull(message = "결제 금액은 필수 값입니다.")
+    private Integer paymentAmount;
+
+    @NotBlank(message = "티켓 유형은 필수 값입니다.")
+    private String ticketType;
+
+    @NotBlank(message = "좌석 정보는 필수 값입니다.")
+    private String seatNames;
 }
